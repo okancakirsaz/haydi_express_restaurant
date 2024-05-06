0b49ca589614128c7a3ee5b8425da8c51ece78f6
okancakirsaz <ocakirsaz@gmail.com>
2024-05-06 20:30:12 +0300

    restaurant name initliazed

--- lib/views/main_view.dart/viewmodel/main_viewmodel.dart
+++ lib/views/main_view.dart/viewmodel/main_viewmodel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:haydi_express_restaurant/core/init/cache/local_keys_enums.dart';
+import 'package:haydi_express_restaurant/views/authentication/models/restaurant_model.dart';
 import 'package:haydi_express_restaurant/views/menu_view/view/menu_view.dart';
 import '../../../../core/base/viewmodel/base_viewmodel.dart';
 import 'package:mobx/mobx.dart';
@@ -13,8 +15,9 @@ abstract class _MainViewModelBase with Store, BaseViewModel {
 
   @override
   init() {
-    //TODO: Get data from cache
-    restaurantName = "Restaurant Name";
+    restaurantName = RestaurantModel.fromJson(
+            localeManager.getJsonData(LocaleKeysEnums.restaurantData.name))
+        .businessName;
   }
 
   @observable
