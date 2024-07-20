a842d488dc55243ec3a936724ce3b7ce256766c8
okancakirsaz <ocakirsaz@gmail.com>
2024-07-21 00:22:02 +0300

    web socket manager url updated

--- lib/core/managers/web_socket_manager.dart
+++ lib/core/managers/web_socket_manager.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-import 'network_manager.dart';
-
-final class WebSocketManager extends NetworkManager {
+final class WebSocketManager {
   static final WebSocketManager instance = WebSocketManager();
 
-  IO.Socket get socket => IO.io(
-      fetchBaseUrl(), IO.OptionBuilder().setTransports(['websocket']).build());
+  IO.Socket get socket => IO.io("http://localhost:3001",
+      IO.OptionBuilder().setTransports(['websocket']).build());
   initializeSocketConnection() {
     try {
       socket.connect();
